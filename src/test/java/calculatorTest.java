9eab5b70c02c44405b08cb666b34677305366905
VigneshGnanavel <167099865+VigneshGnanavel@users.noreply.github.com>
2024-07-25 23:01:59 +0530

    Update calculatorTest.java

--- src/test/java/calculatorTest.java
+++ src/test/java/calculatorTest.java
@@ -6,33 +6,30 @@ import org.junit.Test;
 import CalculatorProject.Calculator;
 
 public class calculatorTest {
-	Calculator calc;
-	@Before
-	public void setUp() {
-		calc = new Calculator();
-	}
-	
-	@Test
-	public void addTwoNumbers() {
-		assertEquals(20, calc.add(10, 10));
-		
-	}
+    Calculator calc;
 
-	@Test
-	public void addThreeNumbers() {
-		assertEquals(20, calc.add(5, 5, 10));
-		
-	}
+    @Before
+    public void setUp() {
+        calc = new Calculator();
+    }
 
-	@Test
-	public void subtractTwoNumbers() {
-		assertEquals(0, calc.subtract(10, 10));
-		
-	}
+    @Test
+    public void addTwoNumbers() {
+        assertEquals(20, calc.add(10, 10));
+    }
 
-	@Test
-	public void subtractThreeNumbers() {
-		assertEquals(0, calc.subtract(20, 10, 10));
-		
-	}
+    @Test
+    public void addThreeNumbers() {
+        assertEquals(20, calc.add(5, 5, 10));
+    }
+
+    @Test
+    public void subtractTwoNumbers() {
+        assertEquals(0, calc.subtract(10, 10));
+    }
+
+    @Test
+    public void subtractThreeNumbers() {
+        assertEquals(0, calc.subtract(20, 10, 10));
+    }
 }
