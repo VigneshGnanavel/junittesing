136e4672cb1513c07fbcff0ca3d9d21428712eb7
VigneshGnanavel <167099865+VigneshGnanavel@users.noreply.github.com>
2024-07-25 23:20:16 +0530

    Update Calculator.java

--- src/main/java/CalculatorProject/Calculator.java
+++ src/main/java/CalculatorProject/Calculator.java
@@ -1,17 +1,6 @@
 package CalculatorProject;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api")
 public class Calculator {
-
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello, calculator app is working!";
-    }
 
     public int add(int num1, int num2) {
         return num1 + num2;
@@ -21,11 +10,11 @@ public class Calculator {
         return num1 + num2 + num3;
     }
 
-    public int subtract(int num1, int num2, int num3) {
-        return num1 - num2 - num3;
-    }
-
     public int subtract(int num1, int num2) {
         return num1 - num2;
+    }
+
+    public int subtract(int num1, int num2, int num3) {
+        return num1 - num2 - num3;
     }
 }
